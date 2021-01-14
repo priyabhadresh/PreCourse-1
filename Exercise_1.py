f4f57825f08c377235609d0faf1d8f76ff01b47f
priyabhadresh <priyabhadresh08@gmail.com>
2021-01-14 08:03:08 -0500

    Exercise 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,22 +1,31 @@
 class myStack:
      def __init__(self):
+          self.s = list()
          
      def isEmpty(self):
-         
+         return len(self.s)!= null
+
      def push(self, item):
-         
+         self.s.append(item)
+
+     # -1 represent last variable (LIFO)
      def pop(self):
-        
+        return self.s.pop(-1)
         
      def peek(self):
+          return self.s[-1]
         
      def size(self):
+          return len(self.s)
          
      def show(self):
+          print(self.s)
          
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
-print(s.show())
+s.show()
+s.push('3')
+s.show()
